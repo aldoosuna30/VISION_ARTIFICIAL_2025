ae6c6422dae9c665ce5e32fd1be0eec87b34f7b7
AldoOsuna <aldomisael.osuna@gmail.com>
2025-05-15 16:06:41 -0600

    000_AldoOsuna
    Modificación código practica 3

--- 002_P3_Histograma/002_P3_Histograma.py
+++ 002_P3_Histograma/002_P3_Histograma.py
@@ -1,24 +1,77 @@
 import cv2
-import numpy
+import matplotlib.pyplot as plt
+import numpy as np
 
-img1 = cv2.imread('3D-Matplotlib.png')
-img_to_yuv = cv2.cvtColor(img1,cv2.COLOR_BGR2YUV)
-img_to_yuv[:,:,0] = cv2.equalizeHist(img_to_yuv[:,:,0])
-hist_equalization_result1 = cv2.cvtColor(img_to_yuv, cv2.COLOR_YUV2BGR)
-cv2.imwrite('result.jpg',hist_equalization_result1)
 
-img2 = cv2.imread('mainlogo.png')
-img_to_yuv = cv2.cvtColor(img2,cv2.COLOR_BGR2YUV)
-img_to_yuv[:,:,0] = cv2.equalizeHist(img_to_yuv[:,:,0])
-hist_equalization_result2 = cv2.cvtColor(img_to_yuv, cv2.COLOR_YUV2BGR)
-cv2.imwrite('result1.jpg',hist_equalization_result2)
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 
-img3 = cv2.imread('mainsvmimage.png')
-img_to_yuv = cv2.cvtColor(img3,cv2.COLOR_BGR2YUV)
-img_to_yuv[:,:,0] = cv2.equalizeHist(img_to_yuv[:,:,0])
-hist_equalization_result3 = cv2.cvtColor(img_to_yuv, cv2.COLOR_YUV2BGR)
-cv2.imwrite('result2.jpg',hist_equalization_result3)
 
-cv2.imshow('result.jpg',hist_equalization_result1)
-cv2.imshow('result1.jpg',hist_equalization_result2)
-cv2.imshow('result2.jpg',hist_equalization_result3)
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+img = cv2.imread('3D-Matplotlib.png', cv2.IMREAD_GRAYSCALE)
+
+
+img_eq = cv2.equalizeHist(img)
+
+
+histx1 = cv2.calcHist([img], [0], None, [256], [0, 256])
+histx2 = cv2.calcHist([img_eq], [0], None, [256], [0, 256])
+
+
+histy1 = np.sum(img, axis=0)
+histy2 = np.sum(img_eq, axis=0)
+
+
+fig = plt.figure(figsize=(14, 6))
+gs = fig.add_gridspec(3, 4)
+
+
+ax1 = fig.add_subplot(gs[0, 0])
+ax1.plot(histx1, color='black')
+ax1.set_title('Histograma X OG')
+ax1.set_xlim([0, 256])
+ax1.set_xticks([])
+ax1.set_yticks([])
+
+
+ax2 = fig.add_subplot(gs[0, 2])
+ax2.plot(histx2, color='black')
+ax2.set_title('Histograma X Ecualizada')
+ax2.set_xlim([0, 256])
+ax2.set_xticks([])
+ax2.set_yticks([])
+
+
+ax3 = fig.add_subplot(gs[1, 0])
+ax3.plot(histy1, color='black')
+ax3.set_title('Histograma Y OG')
+ax3.set_ylim([0, np.max(histy1)])
+ax3.set_yticks([])
+ax3.set_xticks([])
+
+
+ax4 = fig.add_subplot(gs[1, 1])
+ax4.imshow(img, cmap='gray')
+ax4.set_title('Original')
+ax4.axis('off')
+
+
+ax5 = fig.add_subplot(gs[1, 2])
+ax5.plot(histy2, color='black')
+ax5.set_title('Histograma Y Ecualizada')
+ax5.set_ylim([0, np.max(histy2)])
+ax5.set_yticks([])
+ax5.set_xticks([])
+
+
+ax6 = fig.add_subplot(gs[1, 3])
+ax6.imshow(img_eq, cmap='gray')
+ax6.set_title('Result')
+ax6.axis('off')
+
+plt.tight_layout()
+plt.show()
